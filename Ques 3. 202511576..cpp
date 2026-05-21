032e97ea5f358262a3716f95e058b874bbcc2948
safaasmeal <safaasmeal@gmail.com>
2026-05-21 23:18:07 +0200

    Add files via upload

---
+++ Ques 3. 202511576..cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Function to read and disply content from an external text file
+void prinFile (string name) {
+ifstream file (name);
+if(!file ) {
+	cout <<"/n[Error] colud not open " <<name<< " (make sure the file exists)" <<endl;
+	return;
+	
+}
+string concepts
+ while (getline(file,line)) {
+	cout <<line <<endl;
+	
+  }
+  cout <<"----------------/n";
+  
+}
+int main() {
+int choice;
+do {
+
+// Main Menu 
+cout <<"/n===Foundation and History of C++ ===/n";
+cout <<"1.History of c++/n";
+cout <<"2.Learn about c++ concepts/n";
+cout <<"3.EXit/n";
+cout << "Enter choine (1-3): ";
+cin >> choine;
+
+switch (choice) {
+	case 1;
+	print File ("history . txt")
+	int subchoice;
+	//int subchoice;
+	
+	return 0;
+}
+	
+	
+ 
+
+
